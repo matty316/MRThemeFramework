c18f746bcea7706fe3e5e040f22670b39df141c1
Matthew Reed <mattreed1890@gmail.com>
2014-05-26 03:09:36 -0500

    Add button and list styles

--- home.php
+++ home.php
@@ -4,8 +4,14 @@
 
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <h1><?php the_title(); ?></h1>
-    <?php the_excerpt(); ?>
+    <article class="excerpt">
+      <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+      <p class="meta">
+        By: <?php the_author(); ?> on <?php echo the_time('l, F, jS, Y'); ?>
+      </p>
+      <?php the_excerpt(); ?>
+      <a href="<?php the_permalink(); ?>" class="btn">Read More</a>
+    </article>
 
   <?php endwhile; else : ?>
 
