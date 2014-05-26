2fea3d29ed94f174a40642ef64d1d75213baf31f
Matthew Reed <mattreed1890@gmail.com>
2014-05-26 01:10:28 -0500

    Block quote styles and more templates

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div class="container root">
+
+  <h1>Oh, shoot!</h1>
+  <p>
+    There ain't no page for that. Maybe hit up that <a href="<?php bloginfo( 'url' ) ?>">Home Page</a>.
+  </p>
+
+  <div id="root_footer"></div>
+</div>
+
+<?php get_footer(); ?>
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Wordpress theme framework
+# Wordpress Theme Framework
 
 I am making this so I have a custom starting point for any themes I make that uses Sass and Compass.
