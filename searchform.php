359fca7b67464c0d9a7d4ba671229c035173d022
Matthew Reed <mattreed1890@gmail.com>
2014-05-26 21:45:49 -0500

    Customize forms

---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+  <input type="text" class="search-input" name="s" id="s" placeholder="Search..." />
+  <button type="submit" name="submit" class="search-submit"><i class="fa fa-search"></i></button>
+</form>
