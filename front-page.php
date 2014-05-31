d6b128bc7f8ea06ea0e648294f468e0dd7de4221
Matthew Reed <mattreed1890@gmail.com>
2014-05-31 18:32:08 -0500

    Add front page widgets

--- front-page.php
+++ front-page.php
@@ -1,22 +1,37 @@
- <?php get_header(); ?>
-<div class="row">
-  <div class="col-8">
-    <div class="container page-body">
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php get_header(); ?>
+   <section class="front-widgets">
+     <div class="container">
+       <div class="row">
+         <div class="col-4">
+           <?php if ( !dynamic_sidebar( 'front-left') ) : ?>
+             <i class="fa fa-heart"></i>
+             <h3>Super Cute!</h3>
+             <p>
+               Small batch disrupt distillery artisan, Schlitz keffiyeh leggings normcore mlkshk farm-to-table.
+             </p>
+           <?php endif; ?>
+         </div>
+         <div class="col-4">
+           <?php if ( !dynamic_sidebar( 'front-center') ) : ?>
+             <i class="fa fa-star"></i>
+             <h3>I'm a Star!</h3>
+             <p>
+               XOXO wolf pug, paleo McSweeney's Truffaut Pinterest American Apparel drinking vinegar forage fap Williamsburg narwhal sustainable.
+             </p>
+           <?php endif; ?>
+         </div>
+         <div class="col-4">
+           <?php if ( !dynamic_sidebar( 'front-right') ) : ?>
+             <i class="fa fa-code"></i>
+             <h3>Lot's of Code!</h3>
+             <p>
+                Godard drinking vinegar tofu direct trade, chia master cleanse pickled. Trust fund 3 wolf moon direct trade PBR&amp;B Tonx, occupy Pinterest.
+             </p>
+           <?php endif; ?>
+         </div>
+       </div>
+     </div>
+   </section>
 
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
-
-      <?php endwhile; else : ?>
-
-        <h1>Sorry, there is no content to display!</h1>
-
-      <?php endif; ?>
-    </div>
-  </div>
-
-  <?php get_sidebar(); ?>
-
-</div>
 
 <?php get_footer(); ?>
